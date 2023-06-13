e81db935980a64fcd7a468f8d3ef89f2db3e9606
djancool016 <dwijulianto16@gmail.com>
2023-06-13 19:04:35 +0700

    add request body (date) for http request

--- factories/transaction-factory.js
+++ factories/transaction-factory.js
@@ -3,6 +3,7 @@ const {CoaFactory} = require('./coa-factory')
 const {AccountFactory} = require('./account-factory')
 const {StatusLogger, DateFormat} = require('../utils')
 const model = require('../models')
+const {Op} = require('sequelize')
 const { dateToCode } = require('../utils/baseUtils')
 
 class TransactionModel extends BaseModel {
@@ -10,7 +11,7 @@ class TransactionModel extends BaseModel {
         super()
         this.model = model.Transaction
         this.query = {
-            attributes: ['id','id_lkm','trans_code','total','remark'],
+            attributes: ['id','id_lkm','trans_code','trans_date','total','remark'],
             include: [
                 {
                     model: model.Coa,
@@ -36,29 +37,41 @@ class TransactionModel extends BaseModel {
         this.query.order = [['updated_at','DESC']]
         return this.findOne()
     }
-    findByLkm(id_lkm){
-        this.query.where = {id_lkm: id_lkm}
-        return this.findAll()
-    }
-    findByCoa(id_coa){
+    findByCoa(id_coa, id_lkm, start_date, end_date){
         this.query.where = {id_coa: id_coa}
+        this.queryOption(id_lkm, start_date, end_date)
         return this.findAll()
     }
     findByTransCode(trans_code){
         this.query.where = {trans_code: trans_code}
         return this.findAll()
     }
-    findByRegister(id_register){
+    findByRegister(id_register, id_lkm, start_date, end_date){
         this.query.where = {'$coa.register.id$': id_register}
+        this.queryOption(id_lkm, start_date, end_date)
         return this.findAll()
     }
-    findByAccount(id_account){
+    findByAccount(id_account, id_lkm, start_date, end_date){
         this.query.where = {'$coa.account.id$': id_account}
+        this.queryOption(id_lkm, start_date, end_date)
         return this.findAll()
     }
-    findByIds(transactionIds){
+    findByIds(transactionIds, id_lkm, start_date, end_date){
         this.query.where = {id: transactionIds}
+        this.queryOption(id_lkm, start_date, end_date)
         return this.findAll()
+    }
+    queryOption(id_lkm, start_date, end_date){
+
+        if(start_date || end_date){
+
+            start_date = new DateFormat(start_date? start_date : "2000-01-01").toISOString(false)
+            end_date = new DateFormat(end_date? end_date : new Date()).toISOString(false)
+            this.query.where = { ...this.query.where, ['trans_date']: {[Op.between]: [start_date, end_date]}} 
+        }
+        if(id_lkm){
+            this.query.where = { ...this.query.where, ['id_lkm']: id_lkm}
+        }
     }
 }
 
@@ -114,7 +127,7 @@ class TransactionFactory {
             })
     }
 
-    async read({id, id_coa, id_account, id_register, trans_code, findLatest = false, transactionIds = []}){
+    async read({id, id_coa, id_lkm, id_account, id_register, trans_code, findLatest = false, transactionIds = [], start_date, end_date}){
 
         let result
 
@@ -122,13 +135,13 @@ class TransactionFactory {
             result = await this.model.findByPk(id)
         }
         else if(id_coa) {
-            result = await this.model.findByCoa(id_coa)
+            result = await this.model.findByCoa(id_coa, id_lkm, start_date, end_date)
         }
         else if(id_account){
-            result = await this.model.findByAccount(id_account)
+            result = await this.model.findByAccount(id_account, id_lkm, start_date, end_date)
         }
         else if(id_register){
-            result = await this.model.findByRegister(id_register)
+            result = await this.model.findByRegister(id_register, id_lkm, start_date, end_date)
         }
         else if(trans_code){
             result = await this.model.findByTransCode(trans_code)
@@ -137,7 +150,7 @@ class TransactionFactory {
             result = await this.model.findLatestOne()
         }
         else if(transactionIds.length > 0){
-            result = await this.model.findByIds(transactionIds)
+            result = await this.model.findByIds(transactionIds, id_lkm, start_date, end_date)
         }
 
         if(result.status) return result
